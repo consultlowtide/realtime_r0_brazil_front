c0dce1a516655d02599c100e3257ed929f6ac085
Martin Laws <martin.laws@shopify.com>
2020-05-09 15:08:50 -0400

    Removed AppBar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-import AppBar from 'components/AppBar';
 import Footer from 'components/Footer';
 import Typography from 'components/Typography';
 import useStyles from './Layout.styles';
@@ -13,7 +12,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <AppBar />
       <Container className={classes.container} maxWidth="lg" component="main">
         <Typography variant="h2" component="h2">
           <em>
