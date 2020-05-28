cce7464021c909e779b8e833aec0db0664e661c7
Martin Laws <martinblaws@gmail.com>
2020-05-27 23:25:37 -0400

    Updating tooltips

--- src/components/charts/Line/TooltipLine/index.js
+++ src/components/charts/Line/TooltipLine/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-
-import { Typography } from '@material-ui/core';
-import { CANADIAN_PROVINCES } from 'utils';
 import Tooltip from 'components/charts/Tooltip';
 
-const TooltipLine = ({ data }) => (
-  <Tooltip data={data}>
-    <Typography variant="caption">
-      <strong>{CANADIAN_PROVINCES[data?.province]}</strong>
-    </Typography>
-  </Tooltip>
-);
+const TooltipLine = ({ data }) => <Tooltip data={data}></Tooltip>;
 
 export default TooltipLine;
--- src/components/charts/Tooltip/index.js
+++ src/components/charts/Tooltip/index.js
@@ -5,6 +5,7 @@ import Rt from 'components/Rt';
 import Thumb from 'components/Thumb';
 
 import useStyles from './Tooltip.styles';
+import { CANADIAN_PROVINCES } from 'utils';
 
 const RED = '#f9e7e7';
 const GREEN = '#c3f5c3';
@@ -12,21 +13,28 @@ const GREEN = '#c3f5c3';
 const Tooltip = ({ children, data = {} }) => {
   const classes = useStyles();
   const getColor = (number) => (number < 1 ? GREEN : RED);
+  const rtValue = () => data?.yFormatted || data?.y;
 
   return (
     <Paper variant="outlined" elevation={3} className={classes.root}>
       {children}
+      <Typography variant="caption">
+        <strong>{CANADIAN_PROVINCES[data?.province]}</strong>
+      </Typography>
+      <Typography variant="caption">{rtValue().toFixed(2)}</Typography>
       <br />
       <Typography variant="caption">
-        <Rt />: {data?.formattedY || data?.y}
+        High:{' '}
+        <span style={{ color: getColor(data?.high) }}>
+          {data?.high.toFixed(2)}
+        </span>
       </Typography>
       <br />
       <Typography variant="caption">
-        High: <Thumb color={getColor(data?.high)} /> {data?.high}
-      </Typography>
-      <br />
-      <Typography variant="caption">
-        Low: <Thumb color={getColor(data?.low)} /> {data?.low}
+        Low:{' '}
+        <span style={{ color: getColor(data?.low) }}>
+          {data?.low.toFixed(2)}
+        </span>
       </Typography>
     </Paper>
   );
