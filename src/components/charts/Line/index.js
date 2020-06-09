8409b04066d528392ed3438db519edf22ae67925
Martin Laws <martinblaws@gmail.com>
2020-06-09 15:21:58 -0400

    Fixed height issue on top graph province bars

--- src/components/charts/Line/index.js
+++ src/components/charts/Line/index.js
@@ -32,7 +32,7 @@ const CustomSymbol = ({
   borderColor,
   ...rest
 }) => {
-  const height = (rest.datum.high - rest.datum.low) * 100;
+  const height = (rest.datum.high - rest.datum.low) * 100 * 2;
 
   return (
     <g>
@@ -41,7 +41,7 @@ const CustomSymbol = ({
         rx={6}
         x={-6}
         height={height}
-        y={(-(rest.datum.high - rest.datum.low) * 100) / 2}
+        y={-(rest.datum.high - rest.datum.low) * 100}
         fill={
           rest.datum.y.toFixed(2) >= 1
             ? palette.line.customSymbol.range.red
