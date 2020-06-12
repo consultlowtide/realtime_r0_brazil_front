dfa2079a4e63a64871779775a2ed3cca3d49a3af
Martin Laws <martinblaws@gmail.com>
2020-06-12 16:17:40 -0400

    Removed unused code

--- src/components/charts/DeathsLine/TooltipDeathsLine/TooltipDeathsLine.styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(1, 3),
-  },
-}));
-
-export default useStyles;
