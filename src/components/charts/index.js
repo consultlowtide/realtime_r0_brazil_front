002f4d288a21e9ca3d6379bf0bca7a76e1c95760
Martin Laws <martinblaws@gmail.com>
2020-06-12 17:48:19 -0400

    Fixed failing import

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,5 +1,3 @@
-export { default as CasesLine } from './CasesLine';
-export { default as DeathsLine } from './DeathsLine';
 export { default as Line } from './Line';
 export { default as RiskBar } from './RiskBar';
 export { default as RiskList } from './RiskList';
