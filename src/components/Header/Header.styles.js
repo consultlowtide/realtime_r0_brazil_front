31b1bc7a13ce81008e97d93887cc94d3f270ff03
Martin Laws <martinblaws@gmail.com>
2020-06-23 09:17:38 -0400

    latest

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -3,8 +3,19 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   header: {
     display: 'flex',
-    flexDirection: 'row',
+    flexFlow: 'row wrap',
     justifyContent: 'space-between',
+  },
+  logo: {
+    width: '50px',
+    height: '50px',
+  },
+  headerNav: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-between',
+    fontFamily: 'InkInknut Antiqua, serif',
+    width: '340px',
   },
 }));
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 import useStyles from './Header.styles';
 
 function Header() {
@@ -8,10 +9,24 @@ function Header() {
   return (
     <>
       <header className={classes.header}>
-        <div>logo</div>
-        <nav>
-          <Link to="/canada">Canada</Link> <Link to="/ontario">Ontario</Link>{' '}
-          <Link to="/resources">Resources</Link>{' '}
+        <Link to="/">
+          <img
+            src="https://rt-canada.ca/Instagram_Square.png"
+            alt="Rt Canada Logo"
+            className={classes.logo}
+          />
+        </Link>
+        <nav className={classes.headerNav}>
+          <Link to="/">
+            <Typography variant="h4">Canada</Typography>
+          </Link>{' '}
+          <Link to="/ontario">
+            <Typography variant="h4">Ontario</Typography>
+          </Link>{' '}
+          <Link to="/resources">
+            {' '}
+            <Typography variant="h4">Resources</Typography>
+          </Link>{' '}
         </nav>
       </header>
     </>
